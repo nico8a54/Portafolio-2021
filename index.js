a2a1d180f3de64a09d29b55a90e47c585b4c8adb
nico8a54 <nico8a54@hotmail.com>
2021-09-02 13:41:42 -0500

    v1
    Header y footer listos

---
+++ index.js
@@ -0,0 +1,21 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const menuH2 = document.getElementById("menu_h2");
+
+function openMenu() {
+    if (hamburger.style.color === "black") {
+        hamburger.style.color = "white";
+        hamburger.innerText = "menu_open";
+        menu.style.transform = "translateY(0vh)";
+        menuH2.style.transform = "translateY(0vh)";
+        // menuH2.style.transform = "translateY(0vh)";
+      } else {
+        hamburger.style.color = "black";
+        hamburger.innerText = "menu";
+        menu.style.transform = "translateY(-100vh)";
+        menuH2.style.transform = "translateY(-100vh)";
+      }
+    
+}
+
+hamburger.addEventListener("click",openMenu);
